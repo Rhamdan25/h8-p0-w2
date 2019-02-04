6bc07dec77f61feb031670dc70f3a0b4f673be76
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-02-04 09:34:04 +0700

    committing files

--- exercise-4.js
+++ exercise-4.js
@@ -1,9 +1,6 @@
 var tanggal = 21; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1945; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
-var tanggal = 21; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
-var tahun = 1945; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 
 //Tanggal
 if( tanggal>0 && tanggal <=31)
